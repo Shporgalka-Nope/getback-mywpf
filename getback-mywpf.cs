9e33fafff9957c544f66a78e6113d674bb1fe196
Arthur <artre2016@gmail.com>
2026-04-13 20:15:38 +0700

    Preparations for update

--- getback-mywpf.cs
+++ getback-mywpf.cs
@@ -5,26 +5,54 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using CommandLine;
 using Scriban;
+using Scriban.Syntax;
 
 
 string baseDir = AppDomain.CurrentDomain.BaseDirectory;
 string templatePath = Path.Combine(baseDir, "templates");
 
-Parser.Default.ParseArguments<Flags>(args)
-    .WithParsed(RunOptions) //Run this method for custom flags
+Parser.Default.ParseArguments<Flags, OneCOptions, DbOptions>(args)
+    .WithParsed<OneCOptions>(args => RunOneC()) //Run this method for get-1c
+    .WithParsed<DbOptions>(args => RunDB()) //Run this method for get-db
+    .WithParsed<Flags>(RunOptions) //Run this method for custom flags
     .WithNotParsed(RunError); //Run this method for --help and --version
 
+//1C Methods
+void RunOneC()
+{
+    Console.WriteLine("[/] Выгрузка ИС для 1С...");
+    try
+    {
+        //Copy 1C database in current folder here
+    }
+    catch(Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"[ERR] Произошла ошибка при копировании: \n{ex.Message}");
+        Console.ResetColor();
+        return;
+    }
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"[!] ИС выгружена в: {Directory.GetCurrentDirectory()}");
+    Console.ResetColor();
+
+}
+
+void RunDB()
+{
+    //DB logic here
+}
 void RunOptions(Flags flags)
 {
-    Console.WriteLine("[/] Searching for .csproj file...");
+    Console.WriteLine("[/] Поиск файла .csproj...");
     string projFileDir;
     projFileDir = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csproj").First();
     string projName = Path.GetFileNameWithoutExtension(projFileDir);
-    Console.WriteLine($"[!] Found .csproj: {projName}");
-    Console.WriteLine($"[/] Scaffolding for {projName}...");
+    Console.WriteLine($"[!] .csproj Найден: {projName}");
+    Console.WriteLine($"[/] Скаффолдинг {projName}...");
     Directory.CreateDirectory("./Windows/");
 
-    //Copy AuthWindow
+    //Copy Windows
     string[] templates = Directory.GetFiles(templatePath);
     foreach(string template in templates)
     {
@@ -34,7 +62,7 @@ void RunOptions(Flags flags)
         if (rawSBN.HasErrors) 
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine($"[ERR] Skipping: {template}");
+            Console.WriteLine($"[ERR] Пропуск: {template}");
             Console.ResetColor();
             continue;
         }
@@ -52,7 +80,7 @@ void RunOptions(Flags flags)
 
         File.WriteAllText(readoutDir, result);
         Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine($"[!] Scaffolded: {readoutDir}");
+        Console.WriteLine($"[!] Скопированно: {readoutDir}");
         Console.ResetColor();
     }
 }
@@ -61,6 +89,3 @@ void RunError(IEnumerable<Error> errors)
 {
     //Leave empty
 }
-
-
-//Console.WriteLine("Hello World!");
